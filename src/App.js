50355891e987d87dcd5ac0b6e0654e897b791b80
Eduard-Konovka <ed098ua@gmail.com>
2021-11-05 20:46:47 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -11,41 +11,27 @@ class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
-    positivePercentage: `${0}%`,
   };
 
-  setStatistics = option => {
-    this.setState({ [option]: this.state[option] + 1 });
-    setTimeout(() => {
-      this.countTotalFeedback();
-      this.countpositivePercentagePercentage();
-    }, 0);
-  };
+  setStatistics = option => this.setState({ [option]: this.state[option] + 1 });
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value, 0);
 
-    this.setState({ total: good + neutral + bad });
-  };
-
-  countpositivePercentagePercentage = () => {
-    const { good, total } = this.state;
-
-    this.setState({
-      positivePercentage: `${Math.round((good / total) * 100)}%`,
-    });
-  };
+  countpositivePercentagePercentage = () =>
+    `${Math.round((this.state.good / this.countTotalFeedback()) * 100)}%`;
 
   render() {
-    const { good, neutral, bad, total, positivePercentage } = this.state;
+    const { good, neutral, bad } = this.state;
     const statistics = this.setStatistics;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countpositivePercentagePercentage();
 
     return (
       <Container title="Виджет отзывов">
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={Object.keys(this.state).slice(0, 3)}
+            options={Object.keys(this.state)}
             onLeaveFeedback={statistics}
           />
         </Section>
